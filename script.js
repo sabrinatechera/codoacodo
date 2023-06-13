44c381ad898152a20a402c4e30406b7d274e508f
sabrina techera <sabrinatecherasosa@gmail.com>
2023-06-13 17:58:30 -0300

    ultimos cambios

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const cantidadElement = document.getElementById("cantidad");
     const categoriaElement = document.getElementById("categoria");
     const totalPagarElement = document.getElementById("totalPagar");
+    const formulario = document.querySelector("form");
+    formulario.addEventListener("reset", borrarCalculo);
     
     cantidadElement.addEventListener("change", calcularTotal);
     categoriaElement.addEventListener("change", calcularTotal);
   
     function calcularTotal() {
+      const entrada= 200;
       const cantidad = parseInt(cantidadElement.value);
       const categoria = categoriaElement.value;
       let descuento = 0;
@@ -18,12 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
         descuento = 0.5;
       } else if (categoria === "junior") {
         descuento = 0.15;
+      }else{
+        descuento=0
       }
   
-      const totalPagar = cantidad * 200 * (1 - descuento);
+      const totalPagar = cantidad * entrada * (1 - descuento);
       totalPagarElement.textContent = "$" + totalPagar.toFixed(2);
     }
+
+    function borrarCalculo() {
+      totalPagarElement.textContent = "$0";
+    }
+  
     
     calcularTotal();
   });
+  
+
   
